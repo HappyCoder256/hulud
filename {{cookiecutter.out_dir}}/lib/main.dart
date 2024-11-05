5a61e0ee3a92f605758b60bf0c09461e7ef5651c
HappyCoder256 <shamndohjovy256@gmail.com>
2024-11-04 16:19:26 -0800

    Update main.dart

--- {{cookiecutter.out_dir}}/lib/main.dart
+++ {{cookiecutter.out_dir}}/lib/main.dart
@@ -74,6 +74,10 @@ String appDir = "";
 Map<String, String> environmentVariables = {};
 
 void main() async {
+  // Configure the app to go full screen
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([]); // Hides both the status and navigation bars
+
   if (isProduction) {
     // ignore: avoid_returning_null_for_void
     debugPrint = (String? message, {int? wrapWidth}) => null;
